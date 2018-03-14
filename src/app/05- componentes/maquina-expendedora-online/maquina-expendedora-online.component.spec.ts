48277c1e07ebec1f4daef412325ef2bdf793873b
Rene Ricardo <rene.ricardo@datys.cu>
2018-03-14 22:59:52 +0100

    Pruebas al componente Maq-Exp-Online (FakeService)
    Falsificando dependencias con FakeService

---
+++ src/app/05- componentes/maquina-expendedora-online/maquina-expendedora-online.component.spec.ts
@@ -0,0 +1,57 @@
+import { TestBed, async, fakeAsync, ComponentFixture } from "@angular/core/testing";
+import { MaquinaExpendedoraOnlineComponent } from "./maquina-expendedora-online.component";
+import { DebugElement } from "@angular/core";
+import { MaquinaExpendedoraService } from "./maquina-expendedora.service";
+import { of } from "rxjs/observable/of";
+import { OBJETOS } from "../maquina-expendedora-offline/maquina-expendedora";
+import { By } from "@angular/platform-browser";
+
+fdescribe('Componente `maquina-expendedora-online`', () => {
+    let fixt: ComponentFixture<MaquinaExpendedoraOnlineComponent>;
+    let comp: MaquinaExpendedoraOnlineComponent;
+    let debug: DebugElement;
+    let servicio_falso: any;
+
+    beforeEach(() => {
+        servicio_falso = {
+            getObjetos: () => of(OBJETOS)
+        }
+
+        TestBed.configureTestingModule({
+            declarations: [MaquinaExpendedoraOnlineComponent],
+            providers: [{ provide: MaquinaExpendedoraService, useValue: servicio_falso }]
+        }).compileComponents();
+
+        fixt = TestBed.createComponent(MaquinaExpendedoraOnlineComponent);
+        comp = fixt.componentInstance;
+        debug = fixt.debugElement;
+
+        fixt.detectChanges();
+    });
+
+    it('debe inicializar el componente e injectar el servicio_falso', () => {
+        expect(comp).toBeTruthy();
+        comp.ngOnInit();
+        expect(comp.objetos).toBeDefined();
+    });
+
+    it('debe contener un elemento con la clase .status y el contenido `conectado`', () => {
+        const elem = debug.query(By.css('.status')).nativeElement.textContent;
+        expect(elem).toContain('conectado');
+    });
+
+    it('debe renderizar en el HTML la misma cantidad de elementos del servicio', () => {
+        const elementos = debug.queryAll(By.css('.card'));
+        expect(elementos.length).toBe(OBJETOS.length);        
+    });
+
+    it('debe mostrar un cartel mostrando el objeto comprado, depues de dar click en comprar', () => {
+        const boton = debug.query(By.css('.btn')).nativeElement;
+        boton.click();
+        fixt.detectChanges();
+        expect(comp.objComprado).toBeDefined();
+        const msj = debug.query(By.css('.notif')).nativeElement.textContent;
+        expect(msj).toContain(comp.objComprado);
+    });
+
+});
